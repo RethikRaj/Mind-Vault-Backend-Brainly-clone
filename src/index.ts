d43441dcff4fdb550999aaaedf93c76f08fd2ce6
RethikRaj <rethikrajrr@gmail.com>
2025-12-09 18:16:07 +0530

    added database with prisma ORM

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,15 @@
 import express from "express";
 import type { Request, Response } from "express";
 import { Config } from './config/config.js';
+import {prisma} from './lib/prisma.js';
 
 const app = express();
 
-app.get("/", (req : Request, res: Response)=>{
-    res.send("Hello World")
+app.get("/ping", async (req : Request, res: Response)=>{
+    res.send("Ping request succesful");
+
+    const users = await prisma.user.findMany();
+    console.log(users)
 })
 
 app.listen(Config.PORT, ()=>{
